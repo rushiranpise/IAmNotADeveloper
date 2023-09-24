777806d3a543dc306a11fabbd95562f231a90184
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-09-24 08:58:40 +0530

    Update Hook.kt

--- app/src/main/java/xyz/xfqlittlefan/notdeveloper/xposed/Hook.kt
+++ app/src/main/java/xyz/xfqlittlefan/notdeveloper/xposed/Hook.kt
@@ -166,6 +166,15 @@ class Hook : IXposedHookLoadPackage {
                         param.result = "usb"
                     }
                 }
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val propName = param.args[0] as String
+                    val propValue = param.result as String
+                    if (propName == "sys.usb.ffs.ready" && propValue == "0") {
+                        // Property sys.usb.ffs.ready is set to 0
+                    } else if ((propName == "sys.usb.state" || propName == "sys.usb.config" || propName == "persist.sys.usb.reboot.func") && propValue == "usb") {
+                        // Properties sys.usb.state, sys.usb.config, and persist.sys.usb.reboot.func are set to "usb"
+                    }
+                }
             }
         )
     }
